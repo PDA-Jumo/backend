a09c006e67a4cc2dfcd194d1a249a9b500bbba81
Youjun Seo <76039485+SEO-YJ@users.noreply.github.com>
2024-03-21 08:42:54 +0900

    Feat: socket namespace 설정

--- utils/socket/socket.js
+++ utils/socket/socket.js
@@ -5,8 +5,10 @@ const io = new Server({
     origin: "http://localhost:3000",
   },
 });
+const community = io.of("/community");
+const stock = io.of("/stock");
 
-require("./CommunitySocketEvents")(io);
-require("./StockSocketEvents")(io);
+require("./CommunitySocketEvents")(community);
+require("./StockSocketEvents")(stock);
 
 module.exports = io;
