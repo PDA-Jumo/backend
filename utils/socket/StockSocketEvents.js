6e37859cc3a5dac5bd603837a5d5da09a19ac255
seohee99 <imshee99@gmail.com>
2024-03-28 10:22:52 +0900

    Feat: redis 데이터 socket 전송

--- utils/socket/StockSocketEvents.js
+++ utils/socket/StockSocketEvents.js
@@ -2,48 +2,36 @@ require("dotenv").config();
 const redisConnect = require("../../models/redis/redisConnect");
 const subscriber = redisConnect.duplicate();
 
-// redis에서 key 조회
-let channels = [];
-(async () => {
-  try {
-    await subscriber.connect();
-    const keys = await subscriber.keys("*");
-    channels = keys;
-    console.log("CHANNELS : ", channels, channels.length); // 결과 확인
-  } catch (err) {
-    console.error("Redis 작업 중 오류 발생:", err);
-  }
-})();
-
-// 각 방마다 subscribe 하도록
-channels.map((channel) => {
-  subscriber.subscribe(channel);
-
-  subscriber.on("message", (channel, message) => {
-    console.log(`Message from channel ${channel}: ${message}`);
-    const data = JSON.parse(message);
-    console.log(`[${channel}] Stock data updated:`, data);
-  });
-
-  subscriber.subscribe(channel, (msg) => {
-    console.log(msg, "is subscribed data");
-  });
-});
-
 module.exports = function (io) {
-  io.on("connection", async (socket) => {
+  (async () => {
+    try {
+      // redis 에서 key 조회
+      await subscriber.connect();
+      const channels = await subscriber.keys("*");
+      console.log("CHANNELS : ", channels, channels.length);
+
+      // 각 방마다 subscribe
+      channels.forEach((channel) => {
+        subscriber.subscribe(channel, (data) => {
+          io.to(channel).emit("stock_update", data);
+        });
+      });
+    } catch (err) {
+      console.error("Redis 작업 중 오류 발생:", err);
+    }
+  })();
+
+  io.on("connection", (socket) => {
     console.log("a user connected", socket.id);
 
     socket.on("joinRoom", ({ stock_code, user_id }) => {
       // user room에 입장
       socket.join(stock_code);
-
-      subscriber.subscribe(stock_code, (data) => {
-        console.log(`message from ${stock_code} : ${data}`);
-      });
     });
 
-    socket.on("leaveRoom", ({ stock_code, user_id }) => {});
+    socket.on("leaveRoom", ({ stock_code, user_id }) => {
+      socket.leave(stock_code);
+    });
 
     socket.on("disconnect", () => {
       console.log("연결이 해제됐습니다.", socket.id);
